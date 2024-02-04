c1a2eec67dfca5698ccb2640b49418cfb330ff2d
Muhammed Duran <77792756+DuranMuhammed@users.noreply.github.com>
2024-02-04 14:36:33 +0300

    Update HandlingSelectionElements.java

--- HandlingSelectionElements.java
+++ HandlingSelectionElements.java
@@ -1,19 +1,14 @@
 package org.example;
 
-import com.sun.source.tree.AssertTree;
-import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
 public class HandlingSelectionElements {
 
